ca21b5c0fe8ba8a91d44d189908a230afc66a659
Brenox889 <brenomendes889@hotmail.com>
2020-10-27 14:10:28 -0300

    feat: functional application and middlewares

--- Bootcamp GoStack 14/Backend/Node-Concepts/src/index.js
+++ Bootcamp GoStack 14/Backend/Node-Concepts/src/index.js
@@ -1,40 +1,77 @@
 const express  = require('express')
-
+const {uuid, isUuid} = require('uuidv4')
 const app = express()
 app.use(express.json())
 
+const projects = []
+
+function logRequests(request, response, next){  
+
+  const { method, url } = request  //Recebe o metodo passado e a url que esta recebendo esse metodo
+  
+  const logLabel = `[${method.toUpperCase()}] ${url} ` // Recebe uma string com o metodo e o deixa em letras maiusculas e a url
+  
+  console.log(logLabel) // imprime a função anterior
+  
+  return(next) //Libera passagem para o proximo Middleware
+}
+
+function validateProjectId(request, response, next){
+  const {id} = request.params
+
+  if(!isUuid(id)){
+    return response.status(400).json({error:'Invalid project ID.'})
+  }
+}
+
 app.get('/projects', (request, response)=>{
-  const query = request.query
-  console.log(query)
-  return response.json([
-    'Projeto1',
-    'Projeto2'
-  ])
+  const {title} = request.query
+
+  const results = title 
+    ? projects.filter(project => project.title.includes(title))
+    : projects
+  return response.json(results)
 })
 app.post('/projects', (request, response)=>{
   const {title, owner} = request.body
-  console.log(title, owner)
+  // console.log(title, owner)
 
-  return response.json([
-    'Projeto1',
-    'Projeto2',
-    'projeto3'
-  ])
+  const project = { id: uuid(), title, owner}
+  projects.push(project)
+
+  return response.json(project)
 })
-app.put('/projects/:id', (request, response)=>{
-  const params = request.params
-  console.log(params)
-  return response.json([
-    'Projeto4',
-    'Projeto2',
-    'projeto3'
-  ])
+app.put('/projects/:id', validateProjectId, (request, response)=>{
+  const {id} = request.params
+  const {title, owner} = request.body
+
+  const projectIndex = projects.findIndex(project => project.id === id)
+
+  if(projectIndex < 0){
+    return response.status(400).json({error:`The project with id ${id} does not exists`})
+  }
+
+  const project = {
+    id,
+    title,
+    owner,
+  }
+
+  projects[projectIndex] = project
+
+  return response.json(project)
 })
-app.delete('/projects/:id', (request, response)=>{
-  return response.json([
-    'Projeto2',
-    'projeto3'
-])
+app.delete('/projects/:id', validateProjectId, (request, response)=>{
+  const {id} = request.params
+
+  const projectIndex = projects.findIndex(project => project.id === id)
+
+  if(projectIndex < 0){
+    return response.status(204).json({error:`The project with id ${id} does not exists`})
+  }
+  projects.splice(projectIndex, 1)
+
+  return response.send()
 })
 
 app.listen(3333, ()=>{
